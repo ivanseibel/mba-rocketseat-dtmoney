fe03302400e8d355b9ec39e281e90fe3a3df7e84
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-10-12 18:21:10 +0100

    Add phosphor-react dependency and Summary component

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const SummaryContainer = styled.section`
+  /* Layout */
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -5rem;
+`;
+
+interface SummaryCardProps {
+  variant?: "green";
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  /* Layout */
+  padding: 2rem;
+
+  /* Styles */
+  background-color: ${(props) => (props.variant === "green" ? props.theme["green-700"] : props.theme["gray-800"])};
+  border-radius: 6px;
+
+  & header {
+    /* Layout */
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    /* Styles */
+    color: ${(props) => props.theme["gray-300"]};
+  }
+
+  & strong {
+    /* Layout */
+    display: block;
+    margin-top: 1rem;
+
+    /* Styles */
+    font-size: 2rem;
+  }
+`;
