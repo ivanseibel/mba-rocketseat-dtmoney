c4c4468c52cc64c9aad509ec4106327e1a6da4a1
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-11-14 19:39:25 +0000

    Add fetchTransactions function to TransactionsContext for querying transactions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,6 +11,7 @@ interface Transaction {
 
 interface TransactionsContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 export const TransactionsContext = createContext({} as TransactionsContextType);
@@ -18,16 +19,30 @@ export const TransactionsContext = createContext({} as TransactionsContextType);
 export function TransactionsProvider({ children }: { children: ReactNode }) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3333/transactions");
+
+    console.log(query);
+
+    if (query) {
+      url.searchParams.append("q", query);
+    }
+
+    console.log(url);
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+    setTransactions(data);
+  }
+
   useEffect(() => {
-    fetch("http://localhost:3333/transactions")
-      .then((response) => response.json())
-      .then((data) => {
-        setTransactions(data);
-      });
+    fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );
