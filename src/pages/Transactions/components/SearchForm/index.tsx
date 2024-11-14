50a2c8d07f2a321c652a61356738b037bdcb124b
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-11-14 19:39:46 +0000

    Integrate fetchTransactions function in SearchForm for transaction querying

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,7 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import { SearchFormContainer } from "./styles";
 
 const searchFormSchema = z.object({
@@ -11,6 +13,8 @@ const searchFormSchema = z.object({
 type SearchFormData = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
+
   const {
     register,
     handleSubmit,
@@ -19,8 +23,8 @@ export function SearchForm() {
     resolver: zodResolver(searchFormSchema),
   });
 
-  const handleSearchTransactions = (data: SearchFormData): void => {
-    console.log(data);
+  const handleSearchTransactions = async (data: SearchFormData) => {
+    await fetchTransactions(data.query);
   };
 
   return (
