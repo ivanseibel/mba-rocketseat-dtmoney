840d7c447f98a6f9607fd30c112c5592f470cb49
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-11-21 19:57:34 +0000

    Refactor SearchForm to use use-context-selector for fetching transactions

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
 import { useForm } from "react-hook-form";
+import { useContextSelector } from "use-context-selector";
 import * as z from "zod";
 import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import { SearchFormContainer } from "./styles";
-import { useContextSelector } from "use-context-selector";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -15,9 +15,9 @@ type SearchFormData = z.infer<typeof searchFormSchema>;
 export function SearchForm() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
-    (context) => context.fetchTransactions
+    (context) => context.fetchTransactions,
   );
-  
+
   const {
     register,
     handleSubmit,
